270cecc17a564d61b3107ffe42a19a7721226eb0
unknown <Mandar2107>
2024-05-17 21:47:55 +0530

    Assigment 9 done

---
+++ Assigment9/Program.cs
@@ -0,0 +1,54 @@
+﻿using Assigment8;
+
+
+
+
+
+
+namespace assigment9
+{
+
+
+
+    class manger : Employee
+    {
+
+        public manger(double bonus)
+        {
+                this._bonus = bonus;
+
+        }
+        private double _bonus;
+
+        public double bonus
+        {
+            get { return _bonus; }
+            set { _bonus = value; }
+        }
+
+
+      public  void accptman()
+        {
+            base.displayemp();
+
+            Console.WriteLine("Enter the bonus :");
+            _bonus = double.Parse(Console.ReadLine());
+
+
+        }
+
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
