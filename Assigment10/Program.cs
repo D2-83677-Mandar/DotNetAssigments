be99055634a2594da0ed1428c347e501bf37a7c7
unknown <Mandar2107>
2024-05-20 15:05:44 +0530

    Assigment 10 Done .....

---
+++ Assigment10/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using Assigment8;
+
+
+namespace Assigment10
+{
+
+
+
+   public class Superior :Employee
+    {
+        
+        public Superior() {
+
+            designation = "Supervisor";
+        }
+        private int _subordinates;
+
+        public int subbordinates
+        {
+            get { return _subordinates; }
+            set { _subordinates = value; }
+        }
+
+        public Superior(int subordinates)
+        {
+            _subordinates = subordinates;
+        }
+
+        public void accept()
+        {
+            base.acceptEmplyee();
+            Console.WriteLine("Enter the no of subbordinates ");
+            _subordinates = int.Parse(Console.ReadLine());
+
+        }
+        public void display()
+        {
+            base.displayemp();
+            Console.WriteLine("subordinates is :" + _subordinates);
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Superior s = new Superior();
+            s.accept();
+            s.display();
+        }
+    }
+}
